bdb953bba84111a3b29e60e8441785e622fae86c
Ubuntu <ubuntu@ip-172-31-1-219.ec2.internal>
2022-11-23 09:04:26 +0000

    loginとlogoutの切り替え

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   root 'top#index'
   get "top/login_page"
   post "top/login"
-  
+  get  'top/logout'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
