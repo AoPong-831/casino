e57daeee1ddf269c86c9ed046ceb74cdf180f686
2011140060 <ubuntu@ip-172-31-1-219.ec2.internal>
2022-11-30 09:02:24 +0000

    パスワードとパスワード確認

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,12 +10,16 @@ class UsersController < ApplicationController
     def create
         require 'date'
         day = Date.today
-        @user =  User.new(name: params[:user][:name], pass: BCrypt::Password.create(params[:user][:pass]), money: 1000, debt: 0, visits: 1, final_date: day)
+        #@user =  User.new(name: params[:user][:name], pass: BCrypt::Password.create(params[:user][:pass]), money: 1000, debt: 0, visits: 1, final_date: day)
+        @user =  User.new(name: params[:user][:name], password: params[:user][:password], password_confirmation:params[:user][:password_confirmation], money: 1000, debt: 0, visits: 1, final_date: day)
+        #@users = User.new(uid: params[:user][:uid], password: params[:user][:password], password_confirmation:params[:user][:password_confirmation])
         if @user.save
+            #flash[:notice] = ''
             redirect_to '/'
         else
-            #render 'new'
-            redirect_to '/'
+            #flash[:notice] = 'パスワードとパスワード確認が一致しません'
+            render 'new'
+            #redirect_to '/'
         end
     end
     def destroy
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+    attr_accessor :password, :password_confirmation
+    validates :name, presence: true, uniqueness: true
+    validates :password, presence: true, confirmation: true
+    def password=(val)
+        if val.present?
+            self.pass = BCrypt::Password.create(val)
+        end
+        @password = val
+    end
 end
