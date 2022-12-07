3cb8cd499779675471ae4bac32fe4694cef5d741
20111400056d <ubuntu@ip-172-31-93-25.ec2.internal>
2022-12-07 09:37:56 +0000

    アカウントページ 基礎

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   get "top/login_page"
   post "top/login"
   get  'top/logout'
+  
+  get "users/index/:id", to: "users#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
