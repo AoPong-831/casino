031d24aa92da0dea80977d7c7379f57eaf3b4750
2011140056d <2011140056d@ed.duk.kindai.ac.jp>
2023-01-06 08:32:54 +0000

    入力値0以下を制限

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -50,14 +50,20 @@ class UsersController < ApplicationController
         message = "初期エラー発生が発生しましたs"#デフォルトメッセージ
         if params[:type] == "withdraw" then#ATM取引、預入、引出を判断
             if 0 > (@user.money - params[:user][:money].to_i) then#引き出し額が持ち金より多い場合
-                message = "引き出し額が不正です"
+                message = "引き出し額が所持金を超えています"
+            elsif params[:user][:money].to_i <= 0 then#入力額が0以下の場合
+                message = "入力額が不正です"
             else
                 Reception.create(user_id: @user.id, money: params[:user][:money], flag: 1)
                 message = "取引を申請中です"
             end
         elsif params[:type] == "deposit" then
-            Reception.create(user_id: @user.id, money: params[:user][:money], flag: 2)
-            message = "取引を申請中です"
+            if params[:user][:money].to_i <= 0 then#入力額が0以下の場合
+                message = "入力額が不正です"
+            else
+                Reception.create(user_id: @user.id, money: params[:user][:money], flag: 2)
+                message = "取引を申請中です"
+            end
         else#エラー対応
             message = "取引中にエラーが発生しました"
         end
