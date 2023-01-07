bb6416c4c1797919bc12c1fa6e65fc6f924e9663
2011140060 <ubuntu@ip-172-31-1-219.ec2.internal>
2023-01-07 08:23:46 +0000

    ヘッダーにアカウント情報

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,16 @@
 class ApplicationController < ActionController::Base
     
     private
-        def admin
+        def admin#管理者nameの設定
             kanri="takaaki"
             return kanri
         end
         helper_method :admin
+        
+        def header_user(a)#headerにログイン時のアカウント情報を載せるため
+            user_t = User.find_by(name: a)
+            return user_t
+        end
+        helper_method :header_user
 
 end
